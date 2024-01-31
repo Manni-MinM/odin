2b4d41877ce2c444a1843041c3d53bb464224f60
Amirhossein Iravanimanesh <airavanimanesh@gmail.com>
2024-02-01 00:17:41 +0330

    Add redis metrics

--- internal/database/redis.go
+++ internal/database/redis.go
@@ -1,31 +1,33 @@
 package database
 
 import (
-    "fmt"
+	"fmt"
+	metric "github.com/Manni-MinM/odin/internal/pkg/metrics"
+	"time"
 
-    "github.com/Manni-MinM/odin/internal/config"
+	"github.com/Manni-MinM/odin/internal/config"
 
 	"github.com/go-redis/redis"
 )
 
 type RedisDB struct {
-    client    *redis.Client
+	client *redis.Client
 }
 
 func RedisConn(conf config.Redis) (*RedisDB, error) {
-    addr := fmt.Sprintf("%v:%v", conf.Host, conf.Port)
-    client := redis.NewClient(&redis.Options{
-        Addr: addr,
-        Password: conf.Password,
-        DB: conf.DBName,
-    })
+	addr := fmt.Sprintf("%v:%v", conf.Host, conf.Port)
+	client := redis.NewClient(&redis.Options{
+		Addr:     addr,
+		Password: conf.Password,
+		DB:       conf.DBName,
+	})
 
-    err := client.Ping().Err()
-    if err != nil {
-        return nil, err
-    }
+	err := client.Ping().Err()
+	if err != nil {
+		return nil, err
+	}
 
-    return &RedisDB{client}, nil
+	return &RedisDB{client}, nil
 }
 
 func (rdb *RedisDB) getAllKeys() ([]string, error) {
@@ -38,42 +40,59 @@ func (rdb *RedisDB) getAllKeys() ([]string, error) {
 }
 
 func (rdb *RedisDB) GetAllValues() ([]string, error) {
+	getAllValuesTime := time.Now()
 	keys, err := rdb.getAllKeys()
 	if err != nil {
+		metric.MethodCount.WithLabelValues("redis_get_all_values", "failed").Inc()
 		return nil, err
 	}
 
-    if len(keys) == 0 {
-        return []string{}, nil
-    }
+	if len(keys) == 0 {
+		return []string{}, nil
+	}
 
 	values, err := rdb.client.MGet(keys...).Result()
 	if err != nil {
+		metric.MethodCount.WithLabelValues("redis_get_all_values", "failed").Inc()
 		return nil, err
 	}
 
-    valueList := []string{}
-    for _, val := range(values) {
-        valueList = append(valueList, val.(string))
+	valueList := []string{}
+	for _, val := range values {
+		valueList = append(valueList, val.(string))
 	}
+
+	metric.MethodDuration.WithLabelValues("redis_get_all_values_duration").Observe(float64(time.Since(getAllValuesTime)))
+	metric.MethodCount.WithLabelValues("redis_get_all_values", "successful").Inc()
 
 	return valueList, nil
 }
 
 func (rdb *RedisDB) Get(key string) (string, error) {
-    val, err := rdb.client.Get(key).Result()
-    if err == redis.Nil || err != nil {
-        return "", err
-    }
+	getTime := time.Now()
 
-    return val, nil
+	val, err := rdb.client.Get(key).Result()
+	metric.MethodDuration.WithLabelValues("redis_get_duration").Observe(float64(time.Since(getTime)))
+	if err == redis.Nil || err != nil {
+		metric.MethodCount.WithLabelValues("redis_get", "failed").Inc()
+		return "", err
+	}
+
+	metric.MethodCount.WithLabelValues("redis_get", "successful").Inc()
+
+	return val, nil
 }
 
 func (rdb *RedisDB) Set(key string, val string) (string, error) {
-    err := rdb.client.Set(key, val, 0).Err()
-    if err == redis.Nil || err != nil {
-        return "", err
-    }
+	setTime := time.Now()
+	err := rdb.client.Set(key, val, 0).Err()
+	metric.MethodDuration.WithLabelValues("redis_set_duration").Observe(float64(time.Since(setTime)))
+	if err == redis.Nil || err != nil {
+		metric.MethodCount.WithLabelValues("redis_set", "failed").Inc()
+		return "", err
+	}
 
-    return key, nil
+	metric.MethodCount.WithLabelValues("redis_set", "successful").Inc()
+
+	return key, nil
 }
--- internal/model/server_health.go
+++ internal/model/server_health.go
@@ -1,108 +1,108 @@
 package model
 
 import (
-    "time"
-    "encoding/json"
+	"encoding/json"
+	"time"
 
-    "github.com/Manni-MinM/odin/internal/database"
+	"github.com/Manni-MinM/odin/internal/database"
 )
 
 type ServerHealth struct {
-    ID              string      `json:"id"`
-    Address         string      `json:"address"`
-    SuccessCount    uint64      `json:"success_count"`
-    FailureCount    uint64      `json:"failure_count"`
-    LastFailure     *uint64     `json:"last_failure"`
-    CreatedAt       uint64      `json:"created_at"`
+	ID           string  `json:"id"`
+	Address      string  `json:"address"`
+	SuccessCount uint64  `json:"success_count"`
+	FailureCount uint64  `json:"failure_count"`
+	LastFailure  *uint64 `json:"last_failure"`
+	CreatedAt    uint64  `json:"created_at"`
 }
 
 type ServerHealthRepo interface {
-    Create(*ServerHealth) (error)
-    GetAll() (map[string]ServerHealth, error)
-    GetByID(string) (ServerHealth, error)
-    IncrementSuccessByID(string) (error)
-    IncrementFailureByID(string) (error)
+	Create(*ServerHealth) error
+	GetAll() (map[string]ServerHealth, error)
+	GetByID(string) (ServerHealth, error)
+	IncrementSuccessByID(string) error
+	IncrementFailureByID(string) error
 }
 
 type RedisServerHealthRepo struct {
-    db  *database.RedisDB
+	db *database.RedisDB
 }
 
 func NewRedisServerHealthRepo(db *database.RedisDB) *RedisServerHealthRepo {
-    return &RedisServerHealthRepo{db}
+	return &RedisServerHealthRepo{db}
 }
 
-func (r *RedisServerHealthRepo) Create(sh *ServerHealth) (error) {
-    data, err := json.Marshal(sh)
+func (r *RedisServerHealthRepo) Create(sh *ServerHealth) error {
+	data, err := json.Marshal(sh)
 	if err != nil {
 		return err
 	}
 
-    id := string(sh.ID)
-    jsonString := string(data)
+	id := string(sh.ID)
+	jsonString := string(data)
 
-    _, err = r.db.Set(id, jsonString)
-    return err
+	_, err = r.db.Set(id, jsonString)
+	return err
 }
 
 func (r *RedisServerHealthRepo) GetAll() (map[string]ServerHealth, error) {
-    values, err := r.db.GetAllValues()
-    if err != nil {
-        return map[string]ServerHealth{}, err
-    }
+	values, err := r.db.GetAllValues()
+	if err != nil {
+		return map[string]ServerHealth{}, err
+	}
 
-    serverHealthMap := make(map[string]ServerHealth)
-    for _, jsonString := range(values) {
-        var sh ServerHealth
+	serverHealthMap := make(map[string]ServerHealth)
+	for _, jsonString := range values {
+		var sh ServerHealth
 
-        err = json.Unmarshal([]byte(jsonString), &sh)
-        if err != nil {
-            return map[string]ServerHealth{}, err
-        }
+		err = json.Unmarshal([]byte(jsonString), &sh)
+		if err != nil {
+			return map[string]ServerHealth{}, err
+		}
 
-        serverHealthMap[sh.ID] = sh
-    }
+		serverHealthMap[sh.ID] = sh
+	}
 
-    return serverHealthMap, nil
+	return serverHealthMap, nil
 }
 
 func (r *RedisServerHealthRepo) GetByID(id string) (ServerHealth, error) {
-    var sh ServerHealth
+	var sh ServerHealth
 
-    jsonString, err := r.db.Get(id)
-    if err != nil {
-        return ServerHealth{}, err
-    }
+	jsonString, err := r.db.Get(id)
+	if err != nil {
+		return ServerHealth{}, err
+	}
 
-    err = json.Unmarshal([]byte(jsonString), &sh)
-    if err != nil {
-        return ServerHealth{}, err
-    }
+	err = json.Unmarshal([]byte(jsonString), &sh)
+	if err != nil {
+		return ServerHealth{}, err
+	}
 
-    return sh, nil
+	return sh, nil
 }
 
 func (r *RedisServerHealthRepo) IncrementSuccessByID(id string) error {
-    sh, err := r.GetByID(id)
-    if err != nil {
-        return err
-    }
+	sh, err := r.GetByID(id)
+	if err != nil {
+		return err
+	}
 
-    sh.SuccessCount += 1
+	sh.SuccessCount += 1
 
-    return r.Create(&sh)
+	return r.Create(&sh)
 }
 
 func (r *RedisServerHealthRepo) IncrementFailureByID(id string) error {
-    sh, err := r.GetByID(id)
-    if err != nil {
-        return err
-    }
+	sh, err := r.GetByID(id)
+	if err != nil {
+		return err
+	}
 
-    sh.FailureCount += 1
+	sh.FailureCount += 1
 
-    timeNow := uint64(time.Now().Unix())
-    sh.LastFailure = &timeNow
+	timeNow := uint64(time.Now().Unix())
+	sh.LastFailure = &timeNow
 
-    return r.Create(&sh)
+	return r.Create(&sh)
 }
